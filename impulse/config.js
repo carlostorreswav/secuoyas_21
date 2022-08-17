683caba2d6a0911c10743c559a73254f997a2c39
Carlos Torres <carlos.torres@secuoyas.com>
2022-08-17 19:55:58 +0200

    removeConfig

--- impulse/config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDMYukoZfEWN2CwA7Ic-bAlKMe5du-KEt4",
-  authDomain: "musicwall-15470.firebaseapp.com",
-  projectId: "musicwall-15470",
-  storageBucket: "musicwall-15470.appspot.com",
-  messagingSenderId: "408599823461",
-  appId: "1:408599823461:web:80417279c636dcea61328c",
-  measurementId: "G-S86WY125M9",
-}
