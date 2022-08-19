62ceb152fe8378ccb6cd8a59ea882d44c7681b35
Carlos Torres <carlos.torres@secuoyas.com>
2022-08-19 13:55:28 +0200

    ok

--- pages/_document.page.js
+++ pages/_document.page.js
@@ -1,7 +1,25 @@
-import Document from "next/document"
+import Document, { Html, Head, Main, NextScript } from "next/document"
+import Head from "next/head"
 import { ServerStyleSheet } from "styled-components"
 
 export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* link your fonts here */}
+          <link
+            href="http://fonts.cdnfonts.com/css/gilroy-bold"
+            rel="stylesheet"
+          ></link>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
