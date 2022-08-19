c54f8cd0968cce3d80e68b5450313bd53b5e99cb
Carlos Torres <carlos.torres@secuoyas.com>
2022-08-19 13:56:05 +0200

    ok

--- pages/_document.page.js
+++ pages/_document.page.js
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from "next/document"
-import Head from "next/head"
 import { ServerStyleSheet } from "styled-components"
 
 export default class MyDocument extends Document {
